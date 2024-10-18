8651ca9ba86b906c045d60abf23d3f1e16db39d1
Thullyoo <thullyofilho@gmail.com>
2024-10-18 18:04:02 -0300

    feat: add method findByName

--- src/main/java/com/thullyooo/owner_micro_service/repository/OwnerRepository.java
+++ src/main/java/com/thullyooo/owner_micro_service/repository/OwnerRepository.java
@@ -4,8 +4,12 @@ import com.thullyooo.owner_micro_service.domain.owner.Owner;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface OwnerRepository extends JpaRepository<Owner, UUID> {
+
+    Optional<Owner> findByName(String name);
+
 }
