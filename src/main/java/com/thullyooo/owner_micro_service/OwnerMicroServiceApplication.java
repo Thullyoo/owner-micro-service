5e9fad182554ca0855ddc6afb1ea3cea8373ed4e
Thullyoo <thullyofilho@gmail.com>
2024-10-18 15:25:00 -0300

    Initial commit

---
+++ src/main/java/com/thullyooo/owner_micro_service/OwnerMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package com.thullyooo.owner_micro_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwnerMicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OwnerMicroServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thullyooo/owner_micro_service/OwnerMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thullyooo.owner_micro_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OwnerMicroServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
