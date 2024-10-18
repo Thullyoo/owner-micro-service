4b8081e9e0dc2537069c30f6d91897b9682e21fa
Thullyoo <thullyofilho@gmail.com>
2024-10-18 17:17:52 -0300

    fix: addressList on OwnerRequest

--- src/main/java/com/thullyooo/owner_micro_service/domain/owner/DTO/OwnerRequestDTO.java
+++ src/main/java/com/thullyooo/owner_micro_service/domain/owner/DTO/OwnerRequestDTO.java
@@ -1,6 +1,8 @@
 package com.thullyooo.owner_micro_service.domain.owner.DTO;
 
+import com.thullyooo.owner_micro_service.domain.address.DTO.AddressRequestDTO;
+
 import java.util.List;
 
-public record OwnerRequestDTO(String name, String email, String document, String password, List<AddressRequestDTO>) {
+public record OwnerRequestDTO(String name, String email, String document, String password, List<AddressRequestDTO> address) {
 }
