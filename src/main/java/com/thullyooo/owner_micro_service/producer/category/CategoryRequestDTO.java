3d8ce6b8a773928dea42ce4c791075ec80c87717
Thullyoo <thullyofilho@gmail.com>
2024-10-19 18:16:51 -0300

    feat: add all dtos category

---
+++ src/main/java/com/thullyooo/owner_micro_service/producer/category/CategoryRequestDTO.java
@@ -0,0 +1,5 @@
+package com.thullyooo.owner_micro_service.producer.category;
+
+public record CategoryRequestDTO(String description, String name, String ownerId) {
+
+}
---
+++ src/main/java/com/thullyooo/owner_micro_service/producer/category/CategoryUpRequestDTO.java
@@ -0,0 +1,4 @@
+package com.thullyooo.owner_micro_service.producer.category;
+
+public record CategoryUpRequestDTO (String name, String description, String id, String ownerId){
+}
