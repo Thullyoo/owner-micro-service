b4d086ff4ea13b6d22cedb3b39e0a0f8ea21cd85
Thullyoo <thullyofilho@gmail.com>
2024-10-20 18:39:35 -0300

    fix: categoryId for productId

--- src/main/java/com/thullyooo/owner_micro_service/producer/product/ProductUpRequestDTO.java
+++ src/main/java/com/thullyooo/owner_micro_service/producer/product/ProductUpRequestDTO.java
@@ -1,4 +1,4 @@
 package com.thullyooo.owner_micro_service.producer.product;
 
-public record ProductUpRequestDTO(String name, Double price, Long categoryId, String description, String ownerId) {
+public record ProductUpRequestDTO(String name, Double price, Long productId, String description, String ownerId) {
 }
